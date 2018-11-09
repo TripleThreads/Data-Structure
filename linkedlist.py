ded8571abf7c58de7e9fdaeb3383de91cdb24dcc
Segni Habulu <programmers2017@outlook.com>
2018-11-09 17:31:13 +0300

    Add files via upload
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
    
    Example:
    
    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
    Output: 7 -> 0 -> 8
    Explanation: 342 + 465 = 807.

---
+++ linkedlist.py
@@ -0,0 +1,54 @@
+class ListNode:
+ def __init__(self, x):
+     self.val = x
+     self.next = None
+
+class Solution:
+    def addTwoNumbers(self, first_linked, second_linked):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+      
+        remainder = 0
+        head = node = ListNode(None)
+        first = True
+
+        while first_linked or second_linked:
+            
+            l1_val = 0  if first_linked ==  None  else first_linked.val
+            l2_val = 0 if second_linked == None  else second_linked.val
+            _sum =l1_val + l2_val + remainder
+            temp = ListNode( _sum % 10 )
+            remainder = 0 if _sum <=9 else 1
+                
+
+            node.next = temp
+            node = temp
+
+            first_linked = first_linked.next if first_linked != None else None
+            second_linked = second_linked.next if second_linked != None else None
+
+            if remainder != 0 and (not first_linked and not second_linked ):
+                node.next = ListNode(remainder)
+        head = head.next
+        return head
+
+solution = Solution()
+
+node_1 = ListNode(2)
+node_1.next = ListNode(4)
+node_1.next.next = ListNode(3)
+##node_1.next.next.next = ListNode(0)
+node_2 = ListNode(5)
+node_2.next = ListNode(6)
+node_2.next.next = ListNode(4)
+x = solution.addTwoNumbers(node_1, node_2)
+while x:
+    print(x.val)
+    x = x.next
+
+
+
+
